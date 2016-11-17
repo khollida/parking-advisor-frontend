c65d47929632eb0140d9d6bc3ad6e961cbab4f92
khollida <khollida@gmail.com>
2016-11-17 16:27:11 -0500

    New location design
    added sticky footer

--- app/routes/location.js
+++ app/routes/location.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return Ember.$.getJSON('http://192.168.10.155:3000/parking/location/' + params.location_id);
+    return RSVP.hash({
+      citations: Ember.$.getJSON('http://192.168.10.155:3000/parking/location/' + params.location_id + '/total'),
+      geo: Ember.$.getJSON('http://192.168.10.155:3000/geolocation/' + params.location_id)
+    });
+    // return Ember.$.getJSON('http://192.168.10.155:3000/parking/location/' + params.location_id);
   }
 });
