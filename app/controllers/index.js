0483da9c2a6b267207ecc2cc9900cf0041ceaf1a
khollida <khollida@gmail.com>
2016-11-18 22:12:08 -0500

    Removed unnecessary alert

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,7 +5,6 @@ export default Ember.Controller.extend({
     location: '',
     actions: {
       searchSpaces() {
-        // alert(`Saving of the following email address is in progress: ${this.get('location')}`);
         this.transitionToRoute('location', this.get('location'));
         this.set('location', '');
 
